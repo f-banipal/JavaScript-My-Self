06041053969fe0c7298ab475f338ac387c2c6363
Shamash <f.banipal@gmail.com>
2024-02-19 18:05:23 +0100

    ...

--- JavaScript_30_Days/Day4/exercice.js
+++ JavaScript_30_Days/Day4/exercice.js
@@ -2,5 +2,23 @@
 //********************** Exercise: Level 1 *********************/
 //? ***********************************************************/ 
 
-//! 
+//! 1. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 //? ***************************************************************
+
+//      Enter your age: 30
+//      You are old enough to drive.
+
+//      Enter your age:15
+//      You are left with 3 years to drive.
+
+
+
+
+
+//! 2. Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+//? ***************************************************************
+
+//      Enter your age: 30
+//      You are 5 years older than me.
+
+
